660c050fd433653cb74aa29c961a3338cdcef600
Clement Vaillant <clement.vaillant@codit.eu>
2024-05-24 18:55:56 +0200

    Fix home

--- networktrainingwebapp/Controllers/HomeController.cs
+++ networktrainingwebapp/Controllers/HomeController.cs
@@ -74,6 +74,6 @@ namespace networktrainingwebapp.Controllers
         public string PartitionKey { get; set; }
         public string RowKey { get; set; }
         public DateTimeOffset? Timestamp { get; set; }
-        public ETag? ETag { get; set; }
+        public ETag ETag { get; set; }
     }
 }
